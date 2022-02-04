8f4196bd6793e7f36bbd7257c5091a14b2f77b8d
wkyu2kg <wkyu@katanagraph.com>
2022-02-04 19:08:15 +0000

    add modifications to example.cc

--- dev-samples/cpp/example.cc
+++ dev-samples/cpp/example.cc
@@ -14,7 +14,13 @@ class Counter {
  public:
   int count;
 
-  Counter(int init) { count = init; }
+  Counter(int init = 0) { count = init; }
+
+  int Increment() {
+    count += 1;
+    return count;
+  }
+
   /// static factory method
   static Counter *FactoryCreate(int init) { return new Counter(init); }
 
@@ -26,6 +32,15 @@ class Counter {
 };
 /// Declare remote function
 RAY_REMOTE(Counter::FactoryCreate, &Counter::Add);
+
+#if 1
+// Factory function of Counter class.
+static Counter *CreateCounter() {
+    return new Counter();
+};
+RAY_REMOTE(&Counter::Increment, CreateCounter);
+
+#endif
 
 int main(int argc, char **argv) {
   /// initialization
@@ -51,6 +66,48 @@ int main(int argc, char **argv) {
   auto actor_object2 = actor.Task(&Counter::Add).Remote(task_object);
   int actor_task_result2 = *(ray::Get(actor_object2));
   std::cout << "actor_task_result2 = " << actor_task_result2 << std::endl;
+
+  // Create an actor from this class.
+  // `ray::Actor` takes a factory method that can produce
+  // a `Counter` object. Here, we pass `Counter`'s factory function
+  // as the argument.
+  auto counter = ray::Actor(CreateCounter).Remote();
+
+  // Call the actor.
+  auto object_ref = counter.Task(&Counter::Increment).Remote();
+  assert(*object_ref.Get() == 1);
+
+#if 1
+  // Create ten Counter actors.
+  std::vector<ray::ActorHandle<Counter>> counters;
+  for (int i = 0; i < 10; i++) {
+    counters.emplace_back(ray::Actor(CreateCounter).Remote());
+  }
+
+  // Increment each Counter once and get the results. These tasks all happen in
+  // parallel.
+  std::vector<ray::ObjectRef<int>> object_refs;
+  for (ray::ActorHandle<Counter> counter_actor : counters) {
+    object_refs.emplace_back(counter_actor.Task(&Counter::Increment).Remote());
+  }
+  // prints 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+  auto results = ray::Get(object_refs);
+  for (const auto &result : results) {
+    std::cout << *result;
+  }
+
+  // Increment the first Counter five times. These tasks are executed serially
+  // and share state.
+  object_refs.clear();
+  for (int i = 0; i < 5; i++) {
+    object_refs.emplace_back(counters[0].Task(&Counter::Increment).Remote());
+  }
+  // prints 2, 3, 4, 5, 6
+  results = ray::Get(object_refs);
+  for (const auto &result : results) {
+    std::cout << *result;
+  }
+#endif
 
   /// shutdown
   ray::Shutdown();
