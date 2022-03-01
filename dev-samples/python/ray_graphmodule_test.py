4564dca83ca6a3801bd0b0437859263cf5a82078
wkyu2kg <wkyu@katanagraph.com>
2022-03-01 16:13:25 +0000

    need to import in the worker

--- dev-samples/python/ray_graphmodule_test.py
+++ dev-samples/python/ray_graphmodule_test.py
@@ -1,3 +1,4 @@
+import os
 import datasets as ds
 import numpy as np
 import pytest
@@ -7,16 +8,29 @@ import ray.util.collective as collective
 import katana.distributed
 from katana.distributed import Graph
 
-ray.init(namespace='coll', address='auto', _redis_password='5241590000000000', runtime_env={"env_vars": {"PYTHONPATH": "/home/wkyu/katana/master/external/katana/katana_python_build/build/lib.linux-x86_64-3.8:/home/wkyu/katana/master/external/katana/katana_python_build/python:/home/wkyu/katana/master/katana_enterprise_python_build/build/lib.linux-x86_64-3.8:/home/wkyu/katana/master/katana_enterprise_python_build/python:/home/wkyu/katana/master/external/katana/katana_python_build/build/lib.linux-x86_64-3.8:/home/wkyu/katana/master/external/katana/katana_python_build/python:/home/wkyu/katana/katana-enterprise-master/python/test"}})
+# This statement does not work because Graph is a name 
+# for both a local variable and a module
+
+#ray.init(namespace='coll', address='auto', _redis_password='5241590000000000', runtime_env={"py_modules": [Graph], "working_dir": "./"})
+
+# Either of the two statements below can work.
+
+#ray.init(namespace='coll', address='auto', _redis_password='5241590000000000', runtime_env={"env_vars": {"PYTHONPATH": "/home/wkyu/katana/master/external/katana/katana_python_build/build/lib.linux-x86_64-3.8:/home/wkyu/katana/master/external/katana/katana_python_build/python:/home/wkyu/katana/master/katana_enterprise_python_build/build/lib.linux-x86_64-3.8:/home/wkyu/katana/master/katana_enterprise_python_build/python:/home/wkyu/katana/master/external/katana/katana_python_build/build/lib.linux-x86_64-3.8:/home/wkyu/katana/master/external/katana/katana_python_build/python:/home/wkyu/katana/katana-enterprise-master/python/test"}})
+ray.init(namespace='coll', address='auto', _redis_password='5241590000000000', runtime_env={"working_dir": "./"})
 
 @ray.remote
 class Worker:
    def __init__(self):
+       # N.B.: Need to explicitly import a module in the worker since 02/27/22
+       import katana.distributed
+
        katana.distributed.initialize()
 
        self.send = np.ones((4, ), dtype=np.float32)
        self.recv = np.zeros((4, ), dtype=np.float32)
+       print(os.environ["PYTHONPATH"])
        self.graph = Graph("gs://katana-demo-datasets/unit-test-inputs/gnn/tester")
+       self.graph = ds.rdg_dataset_url("gnn_tester", "local")
 
    def setup(self, world_size, rank):
        collective.init_collective_group(world_size, rank, "gloo", "default")
