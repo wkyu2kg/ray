4bb2bd9f95bc44b25833da47bdf7602cc32ba7a8
wkyu2kg <wkyu@katanagraph.com>
2022-03-08 16:45:08 +0000

    katana+raycoll works

--- dev-samples/python/ray_graphcoll_test.py
+++ dev-samples/python/ray_graphcoll_test.py
@@ -4,13 +4,22 @@ import numpy as np
 import pytest
 import ray
 import ray.util.collective as collective
+from pprint import pprint
+
+import socket
+import time
+
+# Import placement group APIs.
+from ray.util.placement_group import (
+    placement_group,
+    placement_group_table,
+    remove_placement_group
+)
 
 import katana.distributed
 from katana.distributed import Graph
 
 from katana.local import DynamicBitset
-
-ray.init(namespace='coll', address='auto', _redis_password='5241590000000000', runtime_env={"working_dir": "./"})
 
 @ray.remote
 class Worker:
@@ -18,17 +27,20 @@ class Worker:
        import katana.distributed
 
        katana.distributed.initialize()
-       self.send = np.ones((4, ), dtype=np.float32)
-       self.recv = np.zeros((4, ), dtype=np.float32)
+       self.send = np.ones((3, ), dtype=np.float32)
+       self.recv = np.zeros((3, ), dtype=np.float32)
        # two statements below are equivalent. use 1st if ds does not load
-       # self.graph = Graph("gs://katana-demo-datasets/unit-test-inputs/gnn/tester")
-       self.graph = ds.rdg_dataset_url("gnn_tester", "local")
+       self.graph = Graph("gs://katana-demo-datasets/unit-test-inputs/gnn/tester")
+       # self.graph = ds.rdg_dataset_url("gnn_tester", "local")
 
    def setup(self, world_size, rank):
        collective.init_collective_group(world_size, rank, "gloo", "default")
        return True
 
    def compute(self):
+       import katana.distributed
+
+       print(socket.gethostname())
        collective.allreduce(self.send, "default")
        return self.send
 
@@ -94,27 +106,38 @@ class Worker:
        obj = ray.put(data_to_sync)
        return obj
 
-# imperative
-num_workers = 2
-workers = []
 
 def test_gluon_vector_comm(datasource):
+    #ctx = ray.init(namespace="gluon", address="auto")
+    ctx = ray.init(namespace='coll', address='auto', _redis_password='5241590000000000', runtime_env={"env_vars": {"PYTHONPATH": "/home/wkyu/katana/master/external/katana/katana_python_build/build/lib.linux-x86_64-3.8:/home/wkyu/katana/master/external/katana/katana_python_build/python:/home/wkyu/katana/master/katana_enterprise_python_build/build/lib.linux-x86_64-3.8:/home/wkyu/katana/master/katana_enterprise_python_build/python:/home/wkyu/katana/master/external/katana/katana_python_build/build/lib.linux-x86_64-3.8:/home/wkyu/katana/master/external/katana/katana_python_build/python:/home/wkyu/katana/katana-enterprise-master/python/test"}})
+
+    print(ray.get_runtime_context().namespace)
+
+    num_workers = 3
+    workers = []
+
+    # Create a placement group.
+    pg = placement_group([{"CPU": 2}, {"CPU": 2}, {"CPU": 2}], strategy="STRICT_SPREAD", name="default_spread", lifetime=None)
+    ray.get(pg.ready())
+
+    pprint(placement_group_table(pg))
+    print(pg.bundle_specs)
+
     for i in range(num_workers):
-       w = Worker.remote()
+       w = Worker.options(placement_group=pg).remote()
+       #w = Worker.remote()
        workers.append(w)
     _options = {
        "group_name": "default",
-       "world_size": 2,
-       "ranks": [0, 1],
+       "world_size": 3,
+       "ranks": [0, 1, 2],
        "backend": "gloo"
     }
     collective.create_collective_group(workers, **_options)
     for w in workers:
         w.graph_vector.remote(datasource)
 
-    # XXX: invoke additional out-of-band tasks for synchronization
+    # XXX: invoke additional out-of-band synchronization
     results = ray.get([w.compute.remote() for w in workers])
     results = ray.get([w.destroy.remote() for w in workers])
-
-
-ray.shutdown()
+    ray.shutdown()
